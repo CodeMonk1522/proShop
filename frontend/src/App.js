621bcafc0782e0b111b21fd556780e7681dd733e
Shubham <shubham@Shubhams-MBP.cable.rcn.com>
2024-09-02 21:20:29 -0500

    frontend first draft done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Outlet } from 'react-router-dom'
 import {Container} from 'react-bootstrap'
 import { Header } from './components/Header'
 import { Footer } from './components/Footer'
@@ -8,7 +9,7 @@ function App() {
       <Header />
       <main className='py-3'>
         <Container>
-          <h1>Welcome to ProShop</h1>
+          <Outlet/>
         </Container>
       </main>
       <Footer/>
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,27 +1,39 @@
 import React from "react";
 import { Navbar, Nav, Container } from "react-bootstrap";
 import { FaShoppingCart, FaUser } from "react-icons/fa";
-import logo from '../assets/logo.png'
+import logo from "../assets/logo.png";
+import { LinkContainer } from "react-router-bootstrap";
+
 export const Header = () => {
   return (
-   <header>
-  <Navbar bg="dark" variant="dark" expand="md" collapseOnSelect>
-    <Container>
-      <Navbar.Brand href="/"><img src={logo} alt="ProShop" srcset="" />ProShop</Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="ms-auto">
-          <Nav.Link href="/cart">
-            <FaShoppingCart />
-          </Nav.Link>
-          <Nav.Link href="/login">
-            <FaUser />
-            Sign In
-          </Nav.Link>
-        </Nav>
-      </Navbar.Collapse>
-    </Container>
-  </Navbar>
-</header>
+    <header>
+      <Navbar bg="dark" variant="dark" expand="md" collapseOnSelect>
+        <Container>
+          <LinkContainer to="/">
+            <Navbar.Brand>
+              <img src={logo} alt="ProShop" srcset="" />
+              ProShop
+            </Navbar.Brand>
+          </LinkContainer>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <LinkContainer to="/cart">
+                <Nav.Link>
+                  <FaShoppingCart />
+                </Nav.Link>
+              </LinkContainer>
+
+              <LinkContainer to="/login">
+                <Nav.Link>
+                  <FaUser />
+                  Sign In
+                </Nav.Link>
+              </LinkContainer>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
   );
 };
